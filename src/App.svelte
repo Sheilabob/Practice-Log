9358e1ab6b7fd30e72870b00c563dc4d11f2498a
Sheila Strahan <smstrahan@gmail.com>
2022-09-15 18:40:25 -0600

    playing with functionality parts

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,48 @@
 <script>
-	export let name;
+	import Modal from './Modal.svelte';
+
+	let people = [
+		{ name: 'ms sheila', subtitle: 'code composer', age:44, id: 1 },
+		{ name: 'holly', subtitle: 'teen trouble', age:14, id: 2 },
+		{ name: 'ben', subtitle: 'little leprechaun', age:8, id: 3 }
+
+	]
+
+	const deletePerson = (id) => {
+		people = people.filter((person) => person.id != id)
+	}
+
+	let signedIn = 'Who are you?';
+
 </script>
 
+<Modal />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input type="text" bind:value={signedIn}>
+
+	{#if signedIn=='Sheila'}
+		{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.subtitle}, {person.age} years old</p>
+			<button on:click={() => deletePerson(person.id)}>delete</button>
+		</div>
+		{:else}
+		<p>There are no people to show . . .</p>
+
+		{/each}
+	{:else if signedIn=='student'}
+		{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.subtitle}, {person.age} years old</p>
+		</div>
+
+		{:else}
+		<p>There are no people to show . . .</p>
+		{/each}
+
+	{/if}
 </main>
 
 <style>
---
+++ src/Modal.svelte
@@ -0,0 +1,15 @@
+<script>
+    let showModal = true;
+</script>
+
+{#if showModal}
+<div class="backdrop">
+    <div class="modal">
+        <p>You met your goal!</p>
+    </div>
+</div>
+{/if}
+
+<style>
+
+</style>
